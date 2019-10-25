920e595e1679866c4d8ce4284b1f27b70fc8f5e9
pavan gunda <pavangunda66@gmail.com>
2019-10-25 20:24:13 +0200

    division script with a test script added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-@app.route('/mul')
-def mul():
+@app.route('/div')
+def div():
     dict = request.args.to_dict()
     A = eval(dict['A'])
     B = eval(dict['B'])
-    result = A*B
+    result = A/B
     return 'result: %s' % result
+
