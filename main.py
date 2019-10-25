18b0632a5be92f65b8ccf2cc136512422c283581
pavan gunda <pavangunda66@gmail.com>
2019-10-25 19:20:56 +0200

    index added that creates a http interface

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
-from flask import Flask, request
+#!/usr/bin/env python
+from flask import Flask, request, redirect, url_for
 
+# create app
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+    if request.method == 'GET':
+        # show html form
+        return '''
+            <form method="post">
+                A: <input type="text" name="A" />
+                B: <input type="text" name="B" />
+                <p><input type="submit" name="operator" value="Add" />
+                <input type="submit" name="operator" value="Sub" />
+                <input type="submit" name="operator" value="Mul" />
+                <input type="submit" name="operator" value="Div" />
+            </form>
+        '''
+    elif request.method == 'POST':
+        # calculate result
+        a = request.form.get('operator')
+        if a == 'Add':
+            A = request.form.get('A')
+            B = request.form.get('B')
+            return redirect(url_for('add', A=A, B=B))
 
+        if a == 'Sub':
+            A = request.form.get('A')
+            B = request.form.get('B')
+            return redirect(url_for('sub', A=A, B=B))
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+        if a == 'Mul':
+            A = request.form.get('A')
+            B = request.form.get('B')
+            return redirect(url_for('mul', A=A, B=B))
 
-
-if __name__ == "__main__":
-    app.run()
+        if a == 'Div':
+            A = request.form.get('A')
+            B = request.form.get('B')
+            return redirect(url_for('div', A=A, B=B))
