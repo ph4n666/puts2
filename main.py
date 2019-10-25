f797a58a663e0afeba4498e365711c89b2cbaec0
pavan gunda <pavangunda66@gmail.com>
2019-10-25 23:08:55 +0200

    subtraction full script with a test script is added

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
+from flask import Flask, request, redirect, url_for
+
+# create app
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        # show html form
+        return '''
+            <form method="post">
+                A: <input type="text" name="A" />
+                B: <input type="text" name="B" />
+                <input type="submit" name="operator" value="Sub" />
+               </form>
+        '''
+    elif request.method == 'POST':
+        # calculate result
+        if a == 'Sub':
+            A = request.form.get('A')
+            B = request.form.get('B')
+            return redirect(url_for('sub', A=A, B=B))
+
+     
+
 @app.route('/sub')
 def sub():
     dict = request.args.to_dict()
@@ -5,3 +32,8 @@ def sub():
     B = eval(dict['B'])
     result = A-B
     return 'result: %s' % result
+
+
+# run app
+if __name__ == '__main__':
+    app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,7 +1,21 @@
- def test_sub(self):
+import unittest
+from cal import app
+
+class MyTestCase(unittest.TestCase):
+    def test_index(self):
+        with app.test_client() as a:
+            get_index = a.get('/')
+            self.assertEqual(get_index._status_code, 200)
+             post_index_sub = a.post('/', data={'A':'6', 'B':'5', 'operator': 'Sub'})
+            self.assertEqual(post_index_sub._status_code, 302)
+
+    def test_sub(self):
         with app.test_client() as a:
             get_sub = a.get('/sub', query_string={'A':'6', 'B':'5'})
             self.assertEqual(get_sub._status_code, 200)
             result_string = get_sub.get_data(as_text=True)
             result = eval(result_string.split('result: ')[1])
             self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
