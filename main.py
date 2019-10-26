f533eb3e54baf1f0d9120a9d0c46fa2c59ac5ccb
pavan gunda <pavangunda66@gmail.com>
2019-10-26 13:13:27 +0200

    division script and a test script is added.

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+#!/usr/bin/env python
+from flask import Flask, request, redirect, url_for
+
+# create app
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        # show html form
+        return '''
+            <form method="post">
+                A: <input type="text" name="A" />
+                B: <input type="text" name="B" />
+                <p>
+                <input type="submit" name="operator" value="Div" />
+            </form>
+        '''
+    elif request.method == 'POST':
+        # calculate result
+        a = request.form.get('operator')
+  
+        if a == 'Div':
+            A = request.form.get('A')
+            B = request.form.get('B')
+            return redirect(url_for('div', A=A, B=B))
+
+
+
 @app.route('/div')
 def div():
     dict = request.args.to_dict()
@@ -6,3 +36,6 @@ def div():
     result = A/B
     return 'result: %s' % result
 
+# run app
+if __name__ == '__main__':
+    app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,4 +1,18 @@
-   def test_div(self):
+  import unittest
+from cal import app
+
+class MyTestCase(unittest.TestCase):
+    def test_index(self):
+        with app.test_client() as a:
+            get_index = a.get('/')
+            self.assertEqual(get_index._status_code, 200)
+       
+            post_index_div = a.post('/', data={'A':'6', 'B':'5', 'operator': 'Div'})
+           
+            self.assertEqual(post_index_div._status_code, 302)
+
+   
+    def test_div(self):
         with app.test_client() as a:
             get_div = a.get('/div', query_string={'A':'6', 'B':'5'})
             self.assertEqual(get_div._status_code, 200)
@@ -6,3 +20,5 @@
             result = eval(result_string.split('result: ')[1])
             self.assertEqual(result, 6/5)
 
+if __name__ == '__main__':
+    unittest.main()
